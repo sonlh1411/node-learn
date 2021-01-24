cd3bf8213a5e88efb504ba0ecf598177a6e15a09
sonlh <sonlh1411@gmail.com>
2021-01-24 22:32:17 +0700

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+app.use(morgan("combined"));
 const port = 3000;
 
 app.get("/", (req, res) => res.send("Hello world"));
